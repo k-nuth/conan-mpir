88d968cccbb52d607cf3948e87f600ec2c4141ed
Fernando pelliccioni <fpelliccioni@gmail.com>
2017-10-20 01:03:53 -0300

    misc

--- build.py
+++ build.py
@@ -4,6 +4,15 @@ import os
 import cpuid
 import copy
 
+def handle_microarchs(microarchs, filtered_builds, settings, options, env_vars, build_requires):
+    print(microarchs)
+    microarchs = list(set(microarchs))
+    print(microarchs)
+
+    for ma in microarchs:
+        opts_copy = copy.deepcopy(options)
+        opts_copy["mpir:microarchitecture"] = ma
+        filtered_builds.append([settings, opts_copy, env_vars, build_requires])
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="bitprim", channel="stable",
@@ -17,22 +26,26 @@ if __name__ == "__main__":
                 and settings["arch"] == "x86_64" \
                 and not ("mpir:shared" in options and options["mpir:shared"]):
 
-            opt1 = copy.deepcopy(options)
-            opt2 = copy.deepcopy(options)
-            opt3 = copy.deepcopy(options)
-            opt4 = copy.deepcopy(options)
+            marchs = ["x86_64", ''.join(cpuid.cpu_microarchitecture()), "haswell", "skylake"]
+            handle_microarchs(marchs, filtered_builds, settings, options, env_vars, build_requires)
 
-            opt1["mpir:microarchitecture"] = "x86_64"
-            opt2["mpir:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
-            opt3["mpir:microarchitecture"] = "haswell"
-            opt4["mpir:microarchitecture"] = "skylake"
+            # opt1 = copy.deepcopy(options)
+            # opt2 = copy.deepcopy(options)
+            # opt3 = copy.deepcopy(options)
+            # opt4 = copy.deepcopy(options)
 
-            filtered_builds.append([settings, opt1, env_vars, build_requires])
-            filtered_builds.append([settings, opt2, env_vars, build_requires])
-            filtered_builds.append([settings, opt3, env_vars, build_requires])
-            filtered_builds.append([settings, opt4, env_vars, build_requires])
+            # opt1["mpir:microarchitecture"] = "x86_64"
+            # opt2["mpir:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
+            # opt3["mpir:microarchitecture"] = "haswell"
+            # opt4["mpir:microarchitecture"] = "skylake"
+
+            # filtered_builds.append([settings, opt1, env_vars, build_requires])
+            # filtered_builds.append([settings, opt2, env_vars, build_requires])
+            # filtered_builds.append([settings, opt3, env_vars, build_requires])
+            # filtered_builds.append([settings, opt4, env_vars, build_requires])
                 
-            # filtered_builds.append([settings, options, env_vars, build_requires])
+            # # filtered_builds.append([settings, options, env_vars, build_requires])
+                
     builder.builds = filtered_builds
 
     builder.run()
