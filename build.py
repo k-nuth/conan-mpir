1b86b88ee4f700198b080a243bfeac9dc6211403
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-01-29 13:49:13 -0300

    better builds microarchitectures [skip ci]

--- build.py
+++ build.py
@@ -4,14 +4,14 @@ import os
 import cpuid
 import copy
 
-def handle_microarchs(microarchs, filtered_builds, settings, options, env_vars, build_requires):
+def handle_microarchs(opt_name, microarchs, filtered_builds, settings, options, env_vars, build_requires):
     print(microarchs)
     microarchs = list(set(microarchs))
     print(microarchs)
 
     for ma in microarchs:
         opts_copy = copy.deepcopy(options)
-        opts_copy["mpir:microarchitecture"] = ma
+        opts_copy[opt_name] = ma
         filtered_builds.append([settings, opts_copy, env_vars, build_requires])
 
 if __name__ == "__main__":
@@ -27,24 +27,7 @@ if __name__ == "__main__":
                 and not ("mpir:shared" in options and options["mpir:shared"]):
 
             marchs = ["x86_64", ''.join(cpuid.cpu_microarchitecture()), "haswell", "skylake"]
-            handle_microarchs(marchs, filtered_builds, settings, options, env_vars, build_requires)
-
-            # opt1 = copy.deepcopy(options)
-            # opt2 = copy.deepcopy(options)
-            # opt3 = copy.deepcopy(options)
-            # opt4 = copy.deepcopy(options)
-
-            # opt1["mpir:microarchitecture"] = "x86_64"
-            # opt2["mpir:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
-            # opt3["mpir:microarchitecture"] = "haswell"
-            # opt4["mpir:microarchitecture"] = "skylake"
-
-            # filtered_builds.append([settings, opt1, env_vars, build_requires])
-            # filtered_builds.append([settings, opt2, env_vars, build_requires])
-            # filtered_builds.append([settings, opt3, env_vars, build_requires])
-            # filtered_builds.append([settings, opt4, env_vars, build_requires])
-                
-            # # filtered_builds.append([settings, options, env_vars, build_requires])
+            handle_microarchs("mpir:microarchitecture", marchs, filtered_builds, settings, options, env_vars, build_requires)
                 
     builder.builds = filtered_builds
 
