430b88f03083806298bd8f85c08e8d166358c5d4
Fernando pelliccioni <fpelliccioni@gmail.com>
2020-01-14 20:50:43 -0300

    fixes for MSVC 16

--- patches/build.vc16/version_info.py
+++ patches/build.vc16/version_info.py
@@ -6,9 +6,9 @@
 #   microsoft compiler version (long)
 #   vcx project tools version
 vs_info = { 'solution':'12',
-            'visual studio':'15',
-            'msvc':'15',
-            'msvc_long':'15.0',
+            'visual studio':'16',
+            'msvc':'16',
+            'msvc_long':'16.0',
             'vcx_tool':'15.0.25914.0',
             'platform_toolset':'141'
           }
